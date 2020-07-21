8c8042a7728dc31bd0fb3f137686a9e2b0175430
Joe Gatt <joe@metlife.com>
2020-07-21 10:33:07 -0400

    updated pip package version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="ocsp-checker",
-    version="1.0.0",
+    version="1.1.0",
     description="Library used to check the OCSP revocation status for a x509 digital certificate.",
     long_description=long_description,
     long_description_content_type="text/markdown",
